778ce6c6b3162cf3c10ca4ea1d5edd381a0e9c12
Fassi Théo <gtheya@gmail.com>
2022-11-13 16:38:58 +0100

    refactor, slash commands
    Co-authored-by: fmaret <fmaret@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -1,96 +1,131 @@
-from discord.ext import commands
-from discord.ext.commands.context import Context
-from discord.message import Message
 from unidecode import unidecode
 
 from os import getenv
 from dotenv import load_dotenv
-from random import choice
 
 from readWords import readWordsJSON
 from Enums import RedLetters, YellowLetters, BlueLetters
 from Classes.game import Game, games
 from utils import *
 
+import interactions
+
 load_dotenv()
 
 DISCORD_TOKEN = getenv("DISCORD_TOKEN")
-PREFIX = ";"
 
 words, dict_words_accents = readWordsJSON("../public/words.json")
 
-bot = commands.Bot(command_prefix=PREFIX)
+bot = interactions.Client(
+    token=DISCORD_TOKEN, intents=interactions.Intents.ALL)
 
 
 @bot.event
 async def on_ready():
     print("HELLO WORLDOU")
 
-@bot.command()
-async def ping(ctx: Context):
-    await ctx.channel.send(choice(words))
 
-@bot.command(aliases=['s', "startGame", "ZEPARTIZEPARTI"])
-async def start(ctx: Context, difficulty: str = "medium"):
-    if doesGameExist(games, ctx.channel.id):
-        await ctx.send("Il y a deja une partie en cours !")
+@bot.command(
+    name="start",
+    description="Start a game of Motus",
+    options=[
+        interactions.Option(
+            name="difficulty",
+            description="The difficulty of the word",
+            required=True,
+            type=interactions.OptionType.STRING,
+            choices=[
+                interactions.Choice(name="Easy", value="easy"),
+                interactions.Choice(name="Medium", value="medium"),
+                interactions.Choice(name="Hard", value="hard")
+            ]
+        )
+    ]
+)
+async def start(ctx: interactions.CommandContext, difficulty: str = "medium"):
+    if doesGameExist(games, ctx.channel_id):
+        await ctx.send("Il y a deja une partie en cours !", ephemeral=True)
         return
-    
+
     random_word = getRandomWordByDifficulty(words, difficulty)
 
-    game = Game(ctx.channel.id, random_word)
+    game = Game(ctx.channel_id, random_word)
 
     game.setRandomCorrectLetters(2)
 
-    await ctx.send("Démarrage de la partie. Mo Mo Motus !")
-    await ctx.send(f"Entrez un mot de {len(random_word)} lettres")
-    await ctx.send(game.correctLettersToString())
-        
-@bot.command()
-async def test(ctx: Context, difficulty: str = "medium"):    
-    random_word = getRandomWordByDifficulty(words, difficulty)
-    await ctx.channel.send(dict_words_accents.get(random_word))
-    await ctx.channel.send(findDefinitions(dict_words_accents.get(random_word)))
+    channel = await ctx.get_channel()
 
-@bot.command()
-async def stop(ctx: Context):
-    if not doesGameExist(games, ctx.channel.id):
+    await ctx.send(f"Démarrage de la partie. Mo Mo Motus !\nEntrez un mot de {len(random_word)} lettres")
+
+    await channel.send(game.correctLettersToString())
+
+
+@bot.command(
+    name="test",
+    description="Send a test word with his definition",
+    options=[
+        interactions.Option(
+                name="difficulty",
+                description="The difficulty of the word",
+                required=True,
+                type=interactions.OptionType.STRING,
+                choices=[
+                    interactions.Choice(name="Easy", value="easy"),
+                    interactions.Choice(name="Medium", value="medium"),
+                    interactions.Choice(name="Hard", value="hard")
+                ]
+        ) 
+    ]
+)
+async def test(ctx: interactions.CommandContext, difficulty: str = "medium"):
+    random_word = getRandomWordByDifficulty(words, difficulty)
+
+    random_word_accent = dict_words_accents.get(random_word)
+
+    embed = generateEmbed(
+        f"Le mot de test est: {random_word_accent}", random_word_accent, bot.me.icon_url)
+
+    await ctx.send(embeds=embed, ephemeral=True)
+
+
+@bot.command(
+    name="stop",
+    description="Stop a game of Motus",
+)
+async def stop(ctx: interactions.CommandContext):
+    if not doesGameExist(games, ctx.channel_id):
         await ctx.send("Il n'y a pas de partie en cours")
         return
-        
-    game = games.get(ctx.channel.id)
 
-    game.stop
+    game = games.get(ctx.channel_id)
+
+    game.delete()
 
     await ctx.send("Partie terminée !")
 
-@bot.event
-async def on_message(message: Message):
+
+@bot.event()
+async def on_message_create(message: interactions.Message):
     msg = unidecode(message.content).lower()
 
     # Pass message by bot
-    if message.author == bot.user:
+    if message.author.id == bot.me.id:
         return
-
-    # Pass message not starting with prefix
-    if msg.startswith(PREFIX):
-        return await bot.process_commands(message)
 
     # Pass message if no active games in channel
-    if not games.get(message.channel.id):
+    if not games.get(message.channel_id):
         return
 
-    random_word = games.get(message.channel.id).word
+    random_word = games.get(message.channel_id).word
 
     # Pass if the length of the word is not the same as the random_word
     if len(msg) != len(random_word):
         return
 
     if not msg in words:
-        return await message.channel.send("Le mot que vous avez écrit n'est pas français.")
+        return await message.reply("Le mot que vous avez écrit n'est pas français.")
 
-
-    game = games.get(message.channel.id)
+    game = games.get(message.channel_id)
 
     # Create a list with every valid letters
     list_letters = list(random_word)
@@ -130,7 +165,7 @@ async def on_message(message: Message):
 
             # Replace - with incorrectly placed letter
             result[i] = YellowLetters[letter]
-            
+
     historique = game.history
     historique.append(result)
 
@@ -139,16 +174,24 @@ async def on_message(message: Message):
     if len(historique) > 2:
         historique.pop(0)
 
-    await message.channel.send(game.historyToString())
+    channel = await message.get_channel()
+
+    await channel.send(game.historyToString())
 
     if msg == game.word:
         game.delete()
-        await message.channel.send(getRandomPhrase(message.author))
-        await message.channel.send(findDefinitions(dict_words_accents.get(game.word)))
-        return
+        # TODO: Embed
+        phrase = getRandomPhrase(message.author)
+
+        embed = generateEmbed(phrase, dict_words_accents.get(
+            game.word), message.author.avatar_url)
+
+        return await channel.send(embeds=embed)
 
     if game.current >= game.limit:
-        await message.channel.send(f"Partie terminée ! Le mot était: {game.word}")
+        embed = generateEmbed(f"Partie terminée ! Le mot était: {game.word}", dict_words_accents.get(
+            game.word), bot.me.icon_url)
+        await channel.send(embeds=embed)
         game.delete()
 
-bot.run(DISCORD_TOKEN)
+bot.start()
--- src/utils.py
+++ src/utils.py
@@ -3,39 +3,46 @@ import requests
 from random import choice
 from bs4 import BeautifulSoup
 
+import interactions
+
+
 def doesGameExist(games, id):
     curr_game = games.get(id, None)
 
     if curr_game is None:
         return False
-        
+
     return True
 
+
 def enumsToString(enum_list):
-    return " ".join(map(lambda l: l.value,enum_list))
+    return " ".join(map(lambda l: l.value, enum_list))
+
 
 def getRandomWordByDifficulty(words, difficulty: str):
     filter_enum = difficulty_filters[difficulty]
 
     filtered_words = list(filter(filter_enum, words))
 
-    random_word = choice(filtered_words)  
+    random_word = choice(filtered_words)
 
     return random_word
-    
+
+
 def getRandomPhrase(user):
     if user.id == 346417942575185922:
         return "Wow bravo Mélanie tu es vraiment très forte, ça me fait très plaisir que tu joues à mon jeu."
 
     phrases = [
-        f"Bravo {user.name}, tu as trouvé le mot.",
-        f"C'est presque trop facile pour toi {user.name}, bravo.",
-        f"Tu as trouvé le mot, mais tu as eu de la chance {user.name}.",
-        f"{user.name} a trouvé la réponse ! Mo Mo Motus !",
-        f"Bravo {user.name}, tu es un champion !"
+        f"Bravo {user.username}, tu as trouvé le mot.",
+        f"C'est presque trop facile pour toi {user.username}, bravo.",
+        f"Tu as trouvé le mot, mais tu as eu de la chance {user.username}.",
+        f"{user.username} a trouvé la réponse ! Mo Mo Motus !",
+        f"Bravo {user.username}, tu es un champion !"
     ]
-    
+
     return choice(phrases)
+
 
 def scrapDefinition(url: str) -> list[str]:
     # Liste des bouts de phrases ou l'on doit refaire un scrapping pour trouver la bonne définition
@@ -55,17 +62,64 @@ def scrapDefinition(url: str) -> list[str]:
         parent_url = "https://fr.wiktionary.org" + definition.find("a")["href"]
         return scrapDefinition(parent_url)
 
-    return definition_text
-        
-def findDefinitions(word: str): 
+    definition_split = definition_text.split("\n")
+
+    return definition_split.pop(0), "\n".join(definition_split), url
+
+
+def findDefinitions(word: str):
     url = f"https://fr.wiktionary.org/w/index.php?search={word}"
     try:
         return scrapDefinition(url)
     except:
-        return "Franchement même le bot a pas trouvé la définition donc c'est pas grave si t'as pas trouvé."
+        return None
+
 
 difficulty_filters = {
     "easy": lambda x: (len(x) < 6),
     "medium": lambda x: (5 < len(x) < 9),
     "hard": lambda x: (8 < len(x))
 }
+
+
+def generateEmbed(phrase, word, avatar_url):
+    definition = findDefinitions(word)
+
+    url = ""
+
+    if definition is not None:
+        definition, example, url = definition
+
+        fields = [
+            interactions.EmbedField(
+                name="Définition",
+                value=definition,
+                inline=False
+            ),
+            interactions.EmbedField(
+                name="Exemple",
+                value=example,
+                inline=False
+            )
+        ]
+    else:
+        fields = [
+            interactions.EmbedField(
+                name="Définition",
+                value="Franchement même le bot a pas trouvé la définition donc c'est pas grave si t'as pas trouvé."
+            )
+        ]
+
+    thumbnail = None
+
+    if avatar_url:
+        thumbnail = interactions.EmbedThumbnail(url=avatar_url)
+
+    embed = interactions.Embed(
+        title=phrase,
+        description=f"Tu as trouvé le mot [**{word}**]({url}) !",
+        fields=fields,
+        thumbnail=thumbnail
+    )
+
+    return embed
